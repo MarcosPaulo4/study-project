66cee86b41e064023be12fb9e0257db7c65a3fda
marcos.paulo <marcospaulo3534q@gmail.com>
2024-09-17 01:10:53 -0300

    feat: first steps

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
